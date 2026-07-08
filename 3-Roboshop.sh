45cf12bbcad2e5dfbb31670b522903f6a0147292
Raviteja.T <ravitejatamada1@gmail.com.com>
2026-07-08 12:14:45 +0530

    Roboshop created

--- 3-Roboshop.sh
+++ 3-Roboshop.sh
@@ -17,19 +17,19 @@ VALIDATE()
 {
 if [ $1 -eq 0 ]
 then 
-echo -e  "$2 is $G"successful"$N" |tee -a $LOG_FILE
+echo -e  "$2 is $G"successful"$N" | tee -a $LOG_FILE
 else
-echo -e "$2 is $R"failure"$N" |tee -a $LOG_FILE
+echo -e "$2 is $R"failure"$N" | tee -a $LOG_FILE
 fi
 }
 cp mongodb.repo /etc/yum.repos.d/mongo.repo
  VALIDATE $? $Y"copying the Repo"$N
 dnf install mongodb-org -y &>>$LOG_FILE
-VALIDATE $? $Y"Installing MongoDB"$N |tee -a $LOG_FILE
+VALIDATE $? $Y"Installing MongoDB"$N | tee -a $LOG_FILE
 systemctl enable mongod &>>$LOG_FILE
-VALIDATE $? $Y"enabling mongod"$N |tee -a $LOG_FILE
+VALIDATE $? $Y"enabling mongod"$N | tee -a $LOG_FILE
 systemctl start mongod &>>$LOG_FILE
-VALIDATE $? $Y"starting mongod"$N |tee -a $LOG_FILE
+VALIDATE $? $Y"starting mongod"$N | tee -a $LOG_FILE
 sed -i 's/127..0.0.1/0.0.0.0/g' /etc/mongod.conf
 systemctl restart mongod &>>$LOG_FILE
 VALIDATE $? $Y"restarting mongodb"$N |tee -a $LOG_FILE   
