d3081a8971850258e63031f8c79ab2d078fcffcc
Raviteja.T <ravitejatamada1@gmail.com.com>
2026-07-08 11:48:31 +0530

    Roboshop created

---
+++ 3-Roboshop.sh
@@ -0,0 +1,28 @@
+USERID=$(id -u)
+if [ $USERID -eq 0 ]
+then 
+echo "you are Running with Root User"
+else
+echo"Please run with Root User"
+exit 1
+fi
+VALIDATE()
+{
+if [ $1 -eq 0 ]
+then 
+echo "$2 is successful"
+then
+echo "$2 is failure"
+fi
+}
+cp mongodb.repo /etc/yum.repos.d/mongo.repo
+Validate $? "copying the Repo"
+dnf install mongodb-org -y 
+VALIDATE $? "Installing MongoDB"
+systemctl enable mongod 
+VALIDATE $? "enabling mongod"
+systemctl start mongod 
+VALIDATE $? "starting mongod"
+sed -i 's/127..0.0.1/0.0.0.0/g' /etc/mongod.conf
+systemctl restart mongod
+VALIDATE $? "restarting mongodb"
